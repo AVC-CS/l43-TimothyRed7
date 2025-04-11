13d66e86409635551024b2070dbf656ef473da92
TimothyRed7 <tdavisreddish@students.avc.edu>
2025-04-11 08:22:18 +0000

    1

--- main.py
+++ main.py
@@ -5,6 +5,13 @@ def main():
     ########################################
     """
     total = 0
+    i=0
+    while(i<0):
+        num=int(input('number'))
+        total=total+num
+        i=i+1
+        print(total)
+    print(total)
 
     ########################################
     # Do not delete the return statement
